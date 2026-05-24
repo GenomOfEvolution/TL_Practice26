68b80f4a89e1d3878e38457063a2a4438cfbdb6a
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2026-05-24 13:06:30 +0300

    ft: начальная миграция, подключение swagger

--- backend/WebApi/API/Program.cs
+++ backend/WebApi/API/Program.cs
@@ -1,32 +1,37 @@
 using Domain.Repositories;
+using Domain.Services;
 using Infrastructure.Foundation;
 using Infrastructure.Foundation.Repositories;
+using Infrastructure.Foundation.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder( args );
 
 builder.Services.AddDbContext<AppDbContext>( options =>
 {
-    options.UseSqlServer( "Server=localhost\\SQLEXPRESS;Database=hotels;Trusted_Connection=True;TrustServerCertificate=True;" );
+    options.UseSqlServer( builder.Configuration.GetConnectionString( "DefaultConnection" ) );
 } );
 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IPropertyService, PropertyService>();
 builder.Services.AddScoped<IPropertyRepository, PropertyRepository>();
 builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
 builder.Services.AddScoped<IRoomTypeRepository, RoomTypeRepository>();
 
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-if ( !app.Environment.IsDevelopment() )
+if ( app.Environment.IsDevelopment() )
 {
-    app.UseExceptionHandler( "/Home/Error" );
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapStaticAssets();
+app.MapControllers();
 
 app.Run();
