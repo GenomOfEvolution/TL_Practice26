23a24f1ed6a1bf9a85a4aea58be42458d5e14b82
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2026-06-07 15:24:03 +0300

    fix: исправления тестов хука

--- frontend/CurrencyConverter/client/src/hooks/useCurrencyConverter.test.tsx
+++ frontend/CurrencyConverter/client/src/hooks/useCurrencyConverter.test.tsx
@@ -3,30 +3,40 @@ import { useCurrencyConverter } from './useCurrencyConverter';
 import { act, renderHook } from '@testing-library/react';
 
 describe('useCurrencyConverter', () => {
-  it('same values replaces with different', () => {
+  it('auto-replaces to when setFrom matches current to', () => {
+    const { result } = renderHook(() => useCurrencyConverter());
+
+    act(() => {
+      result.current.setTo('PLN');
+    });
+
+    expect(result.current.to.code).toBe('PLN');
+
+    act(() => {
+      result.current.setFrom('PLN');
+    });
+
+    expect(result.current.from.code).toBe('PLN');
+    expect(result.current.to.code).not.toBe('PLN');
+  });
+
+  it('swap correctly recalculates result', () => {
     const { result } = renderHook(() => useCurrencyConverter());
 
     act(() => {
       result.current.setFrom('PLN');
-      result.current.setTo('PLN');
-    });
-
-    expect(result.current.from.code !== result.current.to.code);
-  });
-
-  it('on value swap recalculates currency amount', () => {
-    const { result } = renderHook(() => useCurrencyConverter());
-
-    act(() => {
+      result.current.setTo('CAD');
       result.current.setAmount('1');
     });
 
-    const oldAmount = Number(result.current.amount);
+    const oldResult = Number(result.current.result);
+
     act(() => {
       result.current.swap();
     });
 
-    const newAmount = Number(result.current.amount);
-    expect(oldAmount).toEqual(1 / newAmount);
+    const newResult = Number(result.current.result);
+
+    expect(oldResult * newResult).toBeCloseTo(1, 1);
   });
 });
